113e8fc4a51b0cbc9423bc4b2d6de2914bec35d2
Atheena <calayanatheena@gmail.com>
2025-07-28 11:08:34 +0800

    pageNo

--- prc_inout.sql
+++ prc_inout.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2025 at 02:35 AM
+-- Generation Time: Jul 27, 2025 at 07:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `prc_inout`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_logs`
+--
+
+CREATE TABLE `activity_logs` (
+  `id` int(11) NOT NULL,
+  `action_type` enum('incoming','outgoing') NOT NULL,
+  `reference_no` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -84,7 +97,11 @@ CREATE TABLE `incoming` (
 --
 
 INSERT INTO `incoming` (`id`, `ctrlNum`, `source`, `dateRecd`, `timeRe`, `subj`, `attachment`, `stat`, `actionUnit`, `dateRel`, `recipient`, `intial`, `trackingNum`, `created_at`) VALUES
-(5, 'adada', 'daedada', '2025-07-16', '01:07:00', 'dada', 'dada', 'Pending', 'dad', '2025-07-15', 'adad', 'ada', 'adasd', '2025-07-15 17:07:51');
+(5, 'adada', 'daedada', '2025-07-16', '01:09:00', 'dada', 'dada', 'Pending', 'dad', '2025-07-15', 'adad', 'ada', 'adasd', '2025-07-15 17:07:51'),
+(6, '1221321', 'gfs', '0000-00-00', '00:21:00', 'sfs', 'gs', 'Pending', 'afs', '0000-00-00', 'test', 'as', '1232', '2025-07-23 16:19:34'),
+(7, '212558-95', 'sample', '2025-07-24', '02:10:00', 'test subj', 'envelope', 'Released', 'test', '2025-07-23', 'test', 'adfc', '321-9875', '2025-07-23 17:10:56'),
+(8, 'wqw', 'sample', '2025-07-05', '00:46:00', 'test subj', 'envelope', 'Released', 'test', '2025-07-26', 'test', 'qw', '123', '2025-07-24 16:44:24'),
+(9, 'csd', 'sd', '2025-07-12', '01:49:00', 'test subj', 'envelope', 'In Progress', 'test', '2025-07-15', 'adad', 'ad', '12', '2025-07-24 16:49:35');
 
 -- --------------------------------------------------------
 
@@ -112,7 +129,8 @@ INSERT INTO `outgoing` (`id`, `date_received`, `control_number`, `division_secti
 (6, '2025-07-07', '123-965', 'n/a', 'PRC-NCR (Manila)', 'test', 1, 'Ms. Henrietta P. Narvaez', 'Chief ARD'),
 (7, '2025-07-09', 'adada', 'test', 'Koronadal', 'ada', 1, 'Rotelo B. Cabugsa', 'Regional Director'),
 (8, '2025-07-21', 'adada', 'n/a', 'San Fernando, Pampanga', 'saa', 1, 'Mr. Paul H. Aban', 'Officer-in-Charge'),
-(9, '2025-07-01', '121', 'fghj', 'Cagayan de Oro City', 'saa', 122, 'Dir. Julie L. Sabalza', 'Officer-in-Charge');
+(9, '2025-07-01', '121', 'fghj', 'Cagayan de Oro City', 'saa', 122, 'Dir. Julie L. Sabalza', 'Officer-in-Charge'),
+(10, '2025-07-12', '123', 'test', 'Davao City', 'test', 1, 'Dir. Raquel R. Abrantes', 'Regional Director');
 
 -- --------------------------------------------------------
 
@@ -137,6 +155,12 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `activity_logs`
+--
+ALTER TABLE `activity_logs`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `contents`
@@ -169,6 +193,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `activity_logs`
+--
+ALTER TABLE `activity_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `contents`
 --
 ALTER TABLE `contents`
@@ -178,13 +208,13 @@ ALTER TABLE `contents`
 -- AUTO_INCREMENT for table `incoming`
 --
 ALTER TABLE `incoming`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `outgoing`
 --
 ALTER TABLE `outgoing`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
